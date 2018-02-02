0c4002d9f38b467eb96ed54342506a7f04171c66
brettvanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-02-01 19:43:28 -0800

    Update iss_location.py
    See comments in script for detailed description of changes.

--- iss_location.py
+++ iss_location.py
@@ -1,21 +1,39 @@
-import requests 
-from geopy.geocoders import Nominatim #geopy was downloaded from github
+from geopy.geocoders import Nominatim #Imports go in alphabetical order.
+import requests
 
-iss = requests.get(url ="http://api.open-notify.org/iss-now.json") #getting coordinates of ISS from API
-iss = iss.json()
-latitude = (iss['iss_position']['latitude']) #storing latitude of ISS into variable
-longitude = (iss['iss_position']['longitude'])#storing longitude of ISS into variable
-geolocator = Nominatim()
+def get_iss_response(): # Docstrings are added to each function. Script is refactored so each function now only does one thing.
+    '''Function gets a JSON response from an API call to an ISS tracking site.
+    '''
+    iss = requests.get(url ="http://api.open-notify.org/iss-now.json")
+    return iss.json()
 
-def issCountryLocator(latitude,longitude):
-    x = str(latitude) #making into a string
-    y = str(longitude)
-    z = x +',' +y #splicing string together since the function below only takes strings not int
-    location = geolocator.reverse(z) 
+def get_iss_coordinates(iss_response):
+    '''Function obtains latitude and longitude coordinates from return of get_iss_response function.
+    '''
+    latitude = (iss_response['iss_position']['latitude'])
+    longitude = (iss_response['iss_position']['longitude'])
+    return latitude, longitude
+
+def get_country_location(latitude,longitude):
+    '''Function takes latitude and longitude coordinates of ISS location as arguments and returns the country name
+    corresponding to those coordinates.
+    '''
+    transform_lat_long = str(latitude) + ',' + str(longitude) #Refactored for brevity.
+    geolocator = Nominatim()
+    location = geolocator.reverse(transform_lat_long)
     if type(location.address) == str:
-         print(location.address)
+        return(location.address)
     else:
-       print("The ISS is currently over water")
+        return('The ISS is currently over water.')
 
+def run():
+    '''Run function that calls all functions in script. Function prints the country name that the ISS is currently over.
+    '''
+    iss_response = get_iss_response()
+    latitude, longitude = get_iss_coordinates(iss_response)
+    country_location = get_country_location(latitude, longitude)
+    print(country_location)
 
-issCountryLocator(latitude,longitude)
+if __name__ == '__main__': #Makes it so you can import module methods on an individual basis. 
+    run()
+
